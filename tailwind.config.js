c97aab73ec53e724ce8ec0516b0a68390f8ffda3
Aaron <uyscuti-wiki@gmail.com>
2022-11-02 21:48:09 +0530

    add support for mobile screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ module.exports = {
     extend: {
       colors: {
         'duron-mindful-gray': '#c8bfb6',
-        'shade-of-red': '#aa6562',
+        'shade-of-red': '#e2625c',
+        'different-shade-of-red': '#e2625cff',
         'shade-of-orange': '#e66019',
+        'very-light-shade-of-orange': '#f0eae5',
+        'shade-of-green': '#9fbd9a',
+      },
+      backgroundImage: {
+          'academi-tree': "url('../media/ddl-logo-tree-transparent.png')",
       },
     },
   },
