d7c7bf6a482ac76930918db2c7b3a247383135b8
uyscuti-wiki <uyscuti.wiki@gmail.com>
2022-11-10 14:18:16 +0530

    add support for translations and mobile screen

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, g, session
+from flask_babel import Babel
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+babel = Babel(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-@app.route('/index')
 def index():
-    return render_template("index.html")
+    if 'lang' in session:
+        session.pop('lang')
+    return redirect(url_for('ddlanding', lang=get_locale()))
+
+
+@app.route('/<string:lang>')
+def ddlanding(lang=None):
+    if lang and lang in app.config['LANGUAGES']:
+        session['lang'] = lang
+    g.locale = get_locale()
+    return render_template('ddlanding.html')
+
+
+@babel.localeselector
+def get_locale():
+    if 'lang' in session:
+        return session['lang']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.html"],
-  theme: {
-    extend: {
-      colors: {
-        'duron-mindful-gray': '#c8bfb6',
-        'shade-of-red': '#e2625c',
-        'different-shade-of-red': '#e2625cff',
-        'shade-of-orange': '#e66019',
-        'very-light-shade-of-orange': '#f0eae5',
-        'shade-of-green': '#9fbd9a',
-      },
-      backgroundImage: {
-          'academi-tree': "url('../media/ddl-logo-tree-transparent.png')",
-      },
+    content: [
+        "./templates/*.html",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'duron-mindful-gray': '#c8bfb6',
+                'shade-of-red': '#e2625c',
+                'different-shade-of-red': '#e2625cff',
+                'shade-of-orange': '#e66019',
+                'very-light-shade-of-orange': '#f0eae5',
+                'shade-of-green': '#9fbd9a',
+                'shade-of-brown': '#796a58',
+                'different-shade-of-brown': '#574c40',
+            },
+            backgroundImage: {
+                'academi-tree': "url('../media/ddl-logo-tree-transparent.png')",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require("flowbite/plugin"),
+        require('tailwindcss-rtl'),
+    ],
 }
